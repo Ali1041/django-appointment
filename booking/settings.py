d1ade4a9df04a1cadddb60a1be6e1f225a40a17d
Ali Amin <aliamin588@gmail.com>
2025-01-07 22:26:35 +0500

    `Update .gitignore to ignore new files and update database settings in settings.py to use environment variables`

--- booking/settings.py
+++ booking/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -78,8 +83,14 @@ WSGI_APPLICATION = "booking.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": os.getenv("DATABASE_ENGINE", "django.db.backends.postgresql"),
+        "NAME": os.getenv("DATABASE_NAME", ""),
+        "USER": os.getenv("DATABASE_USER", ""),
+        "PASSWORD": os.getenv("DATABASE_PASSWORD", ""),
+        "HOST": os.getenv("DATABASE_HOST", ""),
+        "PORT": os.getenv("DATABASE_PORT", ""),
+        "ATOMIC_REQUESTS": True,
+        "OPTIONS": {"sslmode": "require"},
     }
 }
 
